d14bdb7a66b6af60f08f6fec13e4c1ee066b553e
Wily Duvan Villamil Rey <willyvillamil861@gmail.com>
2026-04-15 18:31:27 -0500

    Update 01_db_schema_setup.sql

--- SQL/01_db_schema_setup.sql
+++ SQL/01_db_schema_setup.sql
@@ -1,1 +1,10 @@
+-- Creación de la base de datos y esquema
+CREATE DATABASE medicare_db
+WITH
+OWNER = postgres
+ENCODING = 'UTF8'
+LC_COLLATE = 'es_CO.UTF-8'
+LC_CTYPE = 'es_CO.UTF-8'
+TEMPLATE = template0;
 
+CREATE SCHEMA IF NOT EXISTS mediCare;
